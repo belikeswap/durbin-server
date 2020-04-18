80bd37113a15b5e0d1b4494d924b13ef55966cb8
Sandipan Dey <sandipandey.12@gmail.com>
2020-04-18 20:50:28 +0530

    server script v0.1 added

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const PORT = 8000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+const users = {};
+const socketToRoom = {};
+
+io.on("connection", (socket) => {
+  socket.on("join room", (roomID) => {
+    if (users[roomID]) {
+      const length = users[roomID].length;
+      if (length === 4) {
+        socket.emit("room full");
+        return;
+      }
+      users[roomID].push(socket.id);
+    } else {
+      users[roomID] = [socket.id];
+    }
+    socketToRoom[socket.id] = roomID;
+    const usersInThisRoom = users[roomID].filter((id) => id !== socket.id);
+
+    socket.emit("all users", usersInThisRoom);
+  });
+
+  socket.on("sending signal", (payload) => {
+    io.to(payload.userToSignal).emit("user joined", {
+      signal: payload.signal,
+      callerID: payload.callerID,
+    });
+  });
+
+  socket.on("returning signal", (payload) => {
+    io.to(payload.callerID).emit("receiving returned signal", {
+      signal: payload.signal,
+      id: socket.id,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    const roomID = socketToRoom[socket.id];
+    let room = users[roomID];
+    if (room) {
+      room = room.filter((id) => id !== socket.id);
+      users[roomID] = room;
+    }
+  });
+});
+
+server.listen(PORT, () => console.log("Durbin Server Online"));
